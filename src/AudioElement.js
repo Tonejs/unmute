6dcdad3d73b88fe50ae1530f4f20f22e8056ca45
tambien <ymann@ymann-macbookpro99.roam.corp.google.com>
2018-07-23 16:45:28 -0400

    playing silent audio for iOS

---
+++ src/AudioElement.js
@@ -0,0 +1,22 @@
+const silentAudio = 'data:audio/mp3;base64,//MkxAAHiAICWABElBeKPL/RANb2w+yiT1g/gTok//lP/W/l3h8QO/OCdCqCW2Cw//MkxAQHkAIWUAhEmAQXWUOFW2dxPu//9mr60ElY5sseQ+xxesmHKtZr7bsqqX2L//MkxAgFwAYiQAhEAC2hq22d3///9FTV6tA36JdgBJoOGgc+7qvqej5Zu7/7uI9l//MkxBQHAAYi8AhEAO193vt9KGOq+6qcT7hhfN5FTInmwk8RkqKImTM55pRQHQSq//MkxBsGkgoIAABHhTACIJLf99nVI///yuW1uBqWfEu7CgNPWGpUadBmZ////4sL//MkxCMHMAH9iABEmAsKioqKigsLCwtVTEFNRTMuOTkuNVVVVVVVVVVVVVVVVVVV//MkxCkECAUYCAAAAFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV'
+
+/**
+ * Plays a silent HTMLAudioElement which causes iOS to unmute even if the mute toggle is on
+ */
+export class AudioElement {
+	constructor(title){
+
+		this.element = document.createElement('audio')
+		this.element.controls = false
+		this.element.preload = 'auto'
+		this.element.loop = false
+		this.element.src = silentAudio
+
+		//set the title that appears on iOS lock screen
+		this.element.title = title
+	}
+
+	click(){
+		this.element.play()
+	}
+}
